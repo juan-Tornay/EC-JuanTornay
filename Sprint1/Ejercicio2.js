6b7abef7754d16eb5615c45c1e6617b66f8b0fd2
Juan Tornay <120737352+juan-Tornay@users.noreply.github.com>
2024-10-02 20:45:10 +0200

    actualizacion

--- Sprint1/Ejercicio2.js
+++ Sprint1/Ejercicio2.js
@@ -1,7 +1,11 @@
+function calcularArea() {
+  // Esto me recoje los numeros en sus cajas
+  const ancho = parseFloat(document.getElementById('ancho').value);
+  const alto = parseFloat(document.getElementById('alto').value);
 
-const calculo= 
-
-function obtenerValor() {
-    var nombre = document.getElementById("ancho").value;
-    alert("El nombre ingresado es: callculo ");
-  }
+  //Aqui declaro la operacion para crear la funcion y hago para que aparezca por pantalla
+      if (1>0) {
+          const area = ancho * alto; // Calcular el área
+          document.getElementById('salida').textContent = `El área del rectángulo es: ${area}`; // Mostrar el resultado
+      } 
+    }
